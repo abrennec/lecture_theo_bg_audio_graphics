b5963cf55de8ed3a93bfe8d025f927a4e54869d7
angela <abrennec@gmail.com>
2020-12-02 10:28:53 +0100

    examples

--- 04_audio_programming/resources/project/sketch.js
+++ 04_audio_programming/resources/project/sketch.js
@@ -1,19 +1,45 @@
 
+let soundFile;
+
+
+function preload() {
+
+    soundFile = loadSound('assets/desk.mp3');
+}
+
+
 function setup() {
 
     createCanvas(400, 200);
-    background(220);
+    background(120);
 
+    //soundFile = loadSound('assets/cow.wav');
+    soundFile.loop();
 }
 
 
 function draw() {
   
+    let volume = map(mouseX, 0, 200, 0, 1);
+    volume = constrain(volume, 0, 1);
+    soundFile.amp(volume); 
+
+    // checkout panning
+    // checkout rate 
+    // checkout reverb
+    
 }
 
 
 function mousePressed() {
 
+    if (soundFile.isPlaying()) {
 
+        soundFile.stop();
+    }
+    else {
+
+        soundFile.play();
+    }
 }
 
