7d22256e85cceade97797c488036820a80a5c239
Seb CTech <sebastian.w94@hotmail.com>
2020-12-02 13:01:53 +0100

    04: fixed the project file back to its original state

--- 04_audio_programming/resources/project/sketch.js
+++ 04_audio_programming/resources/project/sketch.js
@@ -1,37 +1,32 @@
 
 let soundFile;
-let reverbOn = true;
-let reverb;
+
 
 function preload() {
+
     soundFile = loadSound('assets/desk.mp3');
 }
 
 
 function setup() {
 
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(400, 200);
     background(120);
-    reverb = new p5.Reverb();
+
+    //soundFile = loadSound('assets/cow.wav');
     soundFile.loop();
-   // reverb.process(soundFile, 2.0, 1.0);
 }
 
 
 function draw() {
   
-    let volume = map(mouseY, height, 0, 0, 1);
+    let volume = map(mouseX, 0, 200, 0, 1);
     volume = constrain(volume, 0, 1);
+    soundFile.amp(volume); 
 
-    let panning = map(mouseX, 0, width, -1, 1);
-    panning = constrain(panning, -1, 1);
-
-    let rate = map(mouseX, 0, width, 0.1, 2);
-    rate = constrain(rate, 0.1, 2);
-
-    soundFile.amp(volume);
-    //soundFile.pan(panning);
-    //soundFile.rate(rate);
+    // checkout panning
+    // checkout rate 
+    // checkout reverb
     
 }
 
@@ -39,23 +34,12 @@ function draw() {
 function mousePressed() {
 
     if (soundFile.isPlaying()) {
+
         soundFile.stop();
     }
     else {
+
         soundFile.play();
     }
 }
 
-/* 
-function keyPressed(){
-    if(keyCode == 32){
-        if(reverbOn){
-            reverb.disconnect();
-            reverbOn = false;
-        } else {
-            reverb.connect();
-            reverb.process(soundFile, 2, 1);
-            reverbOn = true;
-        }
-    }
-} */
